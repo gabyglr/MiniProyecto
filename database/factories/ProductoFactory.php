e42eb1443bc0a3064c3774fca18e0e5e880d932c
gabyglr <l21271214@tuxtla.tecnm.mx>
2025-04-28 11:57:26 -0600

    Agregando nuevas factories

---
+++ database/factories/ProductoFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Producto;
+use App\Models\Categoria;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ProductoFactory extends Factory
+{
+    protected $model = Producto::class;
+
+    public function definition(): array
+    {
+        return [
+            'nombre' => $this->faker->word(),
+            'descripcion' => $this->faker->sentence(),
+            'precio' => $this->faker->randomFloat(2, 10, 1000),
+            'categoria_id' => Categoria::inRandomOrder()->first()?->id ?? Categoria::factory(),
+        ];
+    }
+}
+
